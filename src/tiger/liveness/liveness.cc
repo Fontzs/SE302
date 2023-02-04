e5b7d52bb0be13c36bb86e273d520a6cbbf0692e
Franz <2simple233@gmail.com>
2023-02-04 23:33:04 +0800

    tiger compiler skeleton

---
+++ src/tiger/liveness/liveness.cc
@@ -0,0 +1,10 @@
+#include "tiger/liveness/liveness.h"
+
+namespace LIVE {
+
+LiveGraph Liveness(G::Graph<AS::Instr>* flowgraph) {
+  // TODO: Put your codes here (lab6).
+  return LiveGraph();
+}
+
+}  // namespace LIVE
